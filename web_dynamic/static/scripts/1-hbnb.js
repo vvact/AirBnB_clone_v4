93dc1bab105b85d30a4dba8487873a90b12d4bca
reggyshicky <reginahshikanda@gmail.com>
2023-10-06 18:07:21 +0300

    checkbox on the amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,12 @@
 $(function () {
-	const amenities = {};
-	$('li input[type=checkbox]').change(
-	  function () {
-		  if (this.checked) {
-			  amenities[this.dataset.name] = this.dataset.id;
-		  } else {
-			  delete amenities[this.dataset.name];
-		  }
-		  $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
-		});
+  const amenities = {};
+  $('li input[type=checkbox]').change(
+    function () {
+      if (this.checked) {
+        amenities[this.dataset.name] = this.dataset.id;
+      } else {
+        delete amenities[this.dataset.name];
+      }
+      $('.amenities h4').text(Object.keys(amenities).sort().join(', '));
+    });
 });
